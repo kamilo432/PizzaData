02feabca5510abec2cfbc45724bbdd6bac26d170
kamilo432 <kamilo432@protonmail.com>
2023-02-20 17:03:01 +0100

    calculator page - in develompemt

---
+++ PizzaData/Pages/Calculator/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PizzaData.Pages.Calculator
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
